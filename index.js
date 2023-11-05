57902a41286af04c435b390548f93dab837252b9
connect-Parvin <parvin.akter0190@gmail.com>
2023-11-05 22:25:03 +0600

    created two collections and edited the database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 
@@ -15,7 +16,10 @@ app.use(express.json());
 app.use(cookieParser())
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bssvhus.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.prs1keb.mongodb.net/tidyDB?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,26 +32,68 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        
+        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const usersCollection = client.db('tidyDB').collection('users');
+        const serviceCollection = client.db('tidyDB').collection('services');
+
+
+
+        // userCollection read 
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find();
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+        // app.get('/user/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: new ObjectId(id) };
+        //     const result = await userCollection.findOne(query);
+        //     res.send(result);
+        // })
+
+
+
+
+        // user collection creat 
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+
+        })
+
+        // service collection data
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find();
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-       
+        // Ensures that the client will close when you finish/error
         // await client.close();
     }
 }
 run().catch(console.dir);
 
 
+
 app.get('/', (req, res) => {
-    res.send('Food and Taste Server side is running on port!')
+    res.send('Tidy Aid Server side is running on port!')
 })
 
 app.listen(port, () => {
-    console.log(`Food and Taste listening on port ${port}`)
+    console.log(`Tidy Cleaning Aid listening on port ${port}`)
 })
 
-// parvinakter0190
-// VDNaN198A3cAnTmX
